8cc2fd83b4466e16f06dd2c018abfc095b1b1f33
melmaganja <melissamaganja@Melissas-MBP.lan>
2024-05-19 16:18:05 -0500

    completed cd_account section

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,6 @@
 # Import the create_cd_account and create_savings_account functions
-# ADD YOUR CODE HERE
+from savings_account import create_savings_account
+
 
 # Define the main function
 def main():
@@ -9,7 +10,7 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    savings_balance = input("enter balance")
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
